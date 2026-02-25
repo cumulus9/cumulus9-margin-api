e05ba798ba5ebadb7f1eaa6f52e662abee9e4b86
Rafik Mrabet <mrabetr@hotmail.com>
2026-02-25 17:32:02 +0000

    updated the js example

--- javascript/cumulus9.js
+++ javascript/cumulus9.js
@@ -3,59 +3,24 @@
 'use-strict'
 
 const axios = require('axios')
-const qs = require('qs')
-
-const getAccessToken = () => {
-    const basicAuthorization = Buffer.from(
-        `${process.env.C9_API_CLIENT_ID}:${process.env.C9_API_CLIENT_SECRET}`,
-    ).toString('base64')
-    return new Promise((res, rej) => {
-        let data = qs.stringify({
-            grant_type: 'client_credentials',
-            scope: 'riskcalc/get',
-        })
-        let config = {
-            method: 'post',
-            url: process.env.C9_API_AUTH_ENDPOINT,
-            headers: {
-                Authorization: `Basic ${basicAuthorization}`,
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            data: data,
-        }
-        axios(config)
-            .then(function (r) {
-                res(r.data)
-            })
-            .catch((e) => {
-                rej(`${e.message || e}`)
-            })
-    })
-}
 
 exports.postPorfolio = (portfolioJson) => {
     return new Promise((res, rej) => {
-        getAccessToken()
-            .then((auth) => {
-                const config = {
-                    method: 'post',
-                    url: `${process.env.C9_API_ENDPOINT}/portfolios`,
-                    headers: {
-                        'Content-Type': 'application/json',
-                        Authorization: 'Bearer ' + auth.access_token,
-                    },
-                    data: portfolioJson,
-                }
-                axios(config)
-                    .then((r) => {
-                        res(r.data)
-                    })
-                    .catch((e) => {
-                        rej(`Cumulus9 API - ${e.message || e}`)
-                    })
+        const config = {
+            method: 'post',
+            url: `${process.env.C9_API_ENDPOINT}/portfolios`,
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${process.env.C9_API_SECRET}`,
+            },
+            data: portfolioJson,
+        }
+        axios(config)
+            .then((r) => {
+                res(r.data)
             })
             .catch((e) => {
-                rej(`Cumulus9 API Token Validation - ${e.message || e}`)
+                rej(`Cumulus9 API - ${e.message || e}`)
             })
     })
 }
